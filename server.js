34718fa646d38d453c96799be688bec6daeed600
Fabricio Flores <fabrifloresg@gmail.com>
2018-01-10 16:49:13 -0500

    Add frontend base

--- server.js
+++ server.js
@@ -16,12 +16,13 @@ app.get('/', function (req, res) {
     res.send('hello world')
 });
 // update according path and version
-app.use('/base/2.4.0/45-fb6443', express.static('/opt/aruba/central/apps/frontend/build'));
-app.use('/guest/2.4.0/45-fb6443', express.static('/opt/aruba/central/apps/guest_ui/build'));
-app.use('/monitoring/2.4.0/45-fb6443', express.static('/opt/aruba/central/apps/monitoring_ui/build'));
-app.use('/msp/2.4.0/45-fb6443', express.static('/opt/aruba/central/apps/msp_ui/build'));
-app.use('/maintenance/2.4.0/45-fb6443', express.static('/opt/aruba/central/apps/maintenance_ui/build'));
-app.use('/reporting/2.4.0/45-fb6443', express.static('/opt/aruba/central/apps/reporting_ui/build'));
+app.use('/base/2.4.0/78-eb4ac3', express.static('/opt/aruba/central/apps/frontend/build'));
+app.use('/base_ui', express.static('/opt/aruba/central/apps/frontend/build'));
+app.use('/guest/2.4.0/78-eb4ac3', express.static('/opt/aruba/central/apps/guest_ui/build'));
+app.use('/monitoring/2.4.0/78-eb4ac3', express.static('/opt/aruba/central/apps/monitoring_ui/build'));
+app.use('/msp/2.4.0/78-eb4ac3', express.static('/opt/aruba/central/apps/msp_ui/build'));
+app.use('/maintenance/2.4.0/78-eb4ac3', express.static('/opt/aruba/central/apps/maintenance_ui/build'));
+app.use('/reporting/2.4.0/78-eb4ac3', express.static('/opt/aruba/central/apps/reporting_ui/build'));
 
 const httpsServer = https.createServer(sslOptions, app);
 httpsServer.listen(443, () => console.log('Server started on port 443!'));
